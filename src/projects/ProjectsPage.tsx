d7f9734e2711f24ab9010bee9a75a8580c4020f0
Peter Loft <peterloft3@gmail.com>
2022-04-29 18:01:29 -0500

    Completed the first basic implementation of the ProjectList component in the ProjectsPage component.

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,13 +1,12 @@
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from './ProjectList';
 
 function ProjectsPage() {
 
   return (
     <>
       <h1>Projects</h1>
-      <pre>
-        {JSON.stringify(MOCK_PROJECTS,null," ")}
-      </pre>
+      <ProjectList projects={MOCK_PROJECTS} />
     </>
     );
 }
